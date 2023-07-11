b639ce6711a5a7ea995fcf3758632457e87dfa8b
Keaton Roux <keaton@codehesion.co.za>
2023-07-11 12:25:29 +0200

    Day 1 - Part 2 Complete

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,7 +1,7 @@
 use std::{fs::File, io::Read};
 
-fn main() {
-    let mut file = File::open("input.txt").expect("Input should be there.");
+fn read_and_sum_calories(file_name: &str) -> Vec<i32> {
+    let mut file = File::open(file_name).expect("Input should be there.");
     let mut contents = String::new();
     file.read_to_string(&mut contents)
         .expect("There should be content in the file");
@@ -10,10 +10,10 @@ fn main() {
 
     println!("Lines in file: {}", lines.len());
 
-    let mut elf_calories: Vec<usize> = Vec::new();
-    let mut elf_count: usize = 0;
+    let mut elf_calories: Vec<i32> = Vec::new();
+    let mut elf_count: i32 = 0;
 
-    let mut calories: usize = 0;
+    let mut calories: i32 = 0;
 
     lines.iter().for_each(|line| {
         if *line == "" {
@@ -21,17 +21,36 @@ fn main() {
             elf_calories.push(calories);
             calories = 0;
         } else {
-            calories += line.parse::<usize>().expect("All lines are strings");
+            calories += line.parse::<i32>().expect("All lines are strings");
         }
     });
 
     println!("Elf count: {}", elf_count);
     println!("Unique calories sets collected: {}", elf_calories.len());
 
+    elf_calories.clone()
+}
+
+fn main() {
+    let mut elf_calories = read_and_sum_calories("input.txt");
+
     let most_calories = elf_calories
+        .clone()
         .into_iter()
         .max()
         .expect("Vector is filled with usize values");
 
     println!("The most calories: {}", most_calories);
+
+    elf_calories.sort_unstable();
+    elf_calories.reverse();
+
+    let mut index = 0;
+    let mut top3 = 0;
+    while index < 3 {
+        top3 += elf_calories[index];
+        index += 1;
+    }
+
+    println!("Top 3 calories: {}", top3);
 }
